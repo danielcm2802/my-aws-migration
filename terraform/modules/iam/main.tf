38c7a55c564e81424febe1c0002328ddae8f4c7c
Daniel Cohen Magary <155140815+danielcm2802@users.noreply.github.com>
2026-06-08 13:50:21 +0300

    Update main.tf

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -143,11 +143,11 @@ resource "aws_security_group" "alb_sg" {
   }
 
   egress {
-    description     = "Forward traffic to EC2 instances"
-    from_port       = 8080
-    to_port         = 8080
-    protocol        = "tcp"
-    security_groups = [aws_security_group.ec2_sg.id]
+    description = "All outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] #checkov:skip=CKV_AWS_382: EC2 requires unrestricted egress for AWS service access
   }
 
   tags = {
